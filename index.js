4f8d5e1fe9a88d2428292595b904dc7cd124bf73
RounakBytes <144313548+RounakBytes@users.noreply.github.com>
2024-01-03 18:00:11 +0530

    Rounakj/add subtract (#2)
    * Add and Subtract Completed
    
    Add and Subtracted Implemented
    
    * Multiply Completed
    
    * Add Implemented 1

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const add = (x, y) => {
+  return x + y;
+};
+
+const substract = (x, y) => {
+  return x - y;
+};
+
+const multiply = (x, y) => {
+  return x * y;
+};
 const divide = (x, y) => {
 
     if (y === 0) {
@@ -7,4 +18,9 @@ const divide = (x, y) => {
 
     return x / y;
 };
+
+console.log(add(5, 10));
+console.log(substract(10, 5));
+console.log(multiply(5, 25));
+console.log(add(5, 10));
 console.log(divide(5, 10));
