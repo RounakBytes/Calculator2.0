4878090103a0b29bbf0abb2267610508c9379b86
RounakBytes <144313548+RounakBytes@users.noreply.github.com>
2024-01-03 17:46:24 +0530

    Multiply Completed

--- index.js
+++ index.js
@@ -6,5 +6,10 @@ const substract = (x, y) => {
   return x - y;
 };
 
-console.log(add(5,10));
-console.log(substract(10,5));
+const multiply = (x, y) => {
+  return x * y;
+};
+
+console.log(add(5, 10));
+console.log(substract(10, 5));
+console.log(multiply(5, 25));
